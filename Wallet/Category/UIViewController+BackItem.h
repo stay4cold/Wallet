ab0117379762f2b856a39660084861556808e9e7
stay4cold <wangchenghao123@126.com>
2018-09-24 11:10:40 +0800

    change the back item

---
+++ Wallet/Category/UIViewController+BackItem.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+BackItem.h
+//  Wallet
+//
+//  Created by 王成浩 on 2018/9/24.
+//  Copyright © 2018年 evan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (BackItem)
+
+@end
---
+++ Wallet/Category/UIViewController+BackItem.m
@@ -0,0 +1,25 @@
+//
+//  UIViewController+BackItem.m
+//  Wallet
+//
+//  Created by 王成浩 on 2018/9/24.
+//  Copyright © 2018年 evan. All rights reserved.
+//
+
+#import "UIViewController+BackItem.h"
+#import <objc/runtime.h>
+
+@implementation UIViewController (BackItem)
+
++ (void)load {
+    Method viewDidLoadMethod = class_getInstanceMethod([self class], @selector(viewDidLoad));
+    Method newViewDidLoadMethod = class_getInstanceMethod([self class], @selector(evan_viewDidLoad));
+    method_exchangeImplementations(viewDidLoadMethod, newViewDidLoadMethod);
+}
+
+- (void)evan_viewDidLoad {
+    [self evan_viewDidLoad];
+    self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"返回" style:UIBarButtonItemStyleDone target:nil action:nil];
+}
+
+@end
